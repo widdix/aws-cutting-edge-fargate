a600f7fb8191fe1ae0880b81c444bc79e970504c
Michael Wittig <michael@widdix.de>
2019-02-26 10:55:10 +0100

    add title

--- src/index.php
+++ src/index.php
@@ -8,6 +8,7 @@ $pdo->exec('CREATE TABLE IF NOT EXISTS votes (id INTEGER AUTO_INCREMENT PRIMARY 
   <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Favorite AWS Architecture</title>
   </head>
   <body>
     <div class="container" style="margin-top: 10px;">
